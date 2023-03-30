b90b6020cfda9967b32c3a3eb7619ac1cb865032
Himel-Tasrif <himel35-1078@diu.edu.bd>
2023-03-30 21:40:56 +0600

    testing purpose

---
+++ Test/himel.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],i,j,n,index;
+    printf("Enter Array size: ");
+    scanf("%d",&n);
+    printf("\n");
+    printf("Enter %d of numbers: ",n);
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter an index you want to remove: ");
+    scanf("%d",&index);
+    for(i=index; i<n; i++)
+    {
+        arr[i] = arr[i+1];
+    }
+
+    printf("The new elements are: ");
+    for(i=0; i<n-1; i++)
+    {
+        printf(" %d",arr[i]);
+    }
+    return 0;
+}
