c6b12fcea1489b4ede633fb123c306732e391c65
Tasrif Nur Himel <125874462+Himel-Tasrif@users.noreply.github.com>
2023-11-01 13:56:17 +0600

    Linear Search

---
+++ Search/linear_search.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],i,n,key;
+    printf("Enter array size: ");
+    scanf("%d",&n);
+    printf("\n");
+
+    printf("Enter %d of numbers: ",n);
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    printf("\nEnter the number you want to search: ");
+    scanf("%d",&key);
+
+    for(i=0; i<n; i++)
+    {
+        if(arr[i] == key)
+        {
+            printf("%d is found at %d position",key,i+1);
+        }
+        //printf("\n");
+    }
+    return 0;
+}
